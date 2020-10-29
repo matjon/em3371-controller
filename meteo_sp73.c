b839281a4e8003034ea45c72cf51d7c8bb69b8ab
Mateusz Jończyk <mat.jonczyk@o2.pl>
2020-10-29 15:54:31 +0100

    meteo_sp73: implement decoding the Weather Station's protocol

--- meteo_sp73.c
+++ meteo_sp73.c
@@ -5,18 +5,110 @@
 
 #include <stdlib.h>
 
+
+/*
+ * Using packed structs with casting may be unsafe on some architectures:
+ * 	https://stackoverflow.com/questions/8568432/is-gccs-attribute-packed-pragma-pack-unsafe
+ * Therefore I'm parsing the packet by hand.
+ */
+
+/*
+ * Accesses three bytes from raw_data.
+ * Returns true if at least some data is present.
+ */
+bool decode_single_measurement(struct device_single_measurement *measurement,
+		const unsigned char *raw_data)
+{
+	if (raw_data[0] == 0xff && raw_data[1] == 0xff && raw_data[2] == 0xff) {
+		measurement->humidity = DEVICE_INCORRECT_HUMIDITY;
+		measurement->temperature = DEVICE_INCORRECT_TEMPERATURE;
+		return false;
+	}
+
+	if (raw_data[0] == 0xff && raw_data[1] == 0xff) {
+		measurement->temperature = DEVICE_INCORRECT_TEMPERATURE;
+		fprintf(stderr, "Weird: measurement contains humidity, but not temperature.\n");
+	} else {
+		// Trying to be endianness-agnostic
+		int raw_temperature = raw_data[0] + ((int)raw_data[1]) * 256;
+		measurement->raw_temperature = raw_temperature;
+
+		// The reverse engineering documentation contains the following formula for
+		// calculating the real value of the temperature:
+		// 	 f(x) = 0,0553127*x - 67,494980
+		// The origin of both numbers is, however, not described. They may
+		// have been obtained from lineal regression or something similar.
+		//
+		// It may be possible that the device sends the value in degrees Fahrenheit
+		// multiplied by 10 with some offset.
+		//
+		// TODO: obtain more accurate formula
+		measurement->temperature = 0.0553127 * (float)raw_temperature - 67.494980;
+	}
+
+	if (raw_data[2] == 0xff) {
+		measurement->humidity = DEVICE_INCORRECT_HUMIDITY;
+		fprintf(stderr, "Weird: measurement contains temperature but not humidity.\n");
+	} else {
+		measurement->humidity = raw_data[2];
+	}
+
+	return true;
+}
+
+bool decode_single_sensor_data(struct device_single_sensor_data *out,
+		const unsigned char *raw_data)
+{
+	bool have_current_data = decode_single_measurement(&(out->current), raw_data);
+	bool have_historical1_data = decode_single_measurement(&(out->historical1), raw_data + 3);
+	bool have_historical2_data = decode_single_measurement(&(out->historical2), raw_data + 6);
+
+	out->any_data_present = have_current_data || have_historical1_data || have_historical2_data;
+
+	return out->any_data_present;
+}
+
+void decode_sensor_state(struct device_sensor_state *state, const unsigned char *received_packet,
+		const size_t received_packet_size)
+{
+	if (received_packet_size <= 61) {
+		fprintf(stderr, "Packet is too short!\n");
+		return;
+	}
+
+	decode_single_sensor_data(&(state->station_sensor), received_packet + 22);
+	for (int i = 0; i < 3; i++) {
+		decode_single_sensor_data(&(state->remote_sensors[i]), received_packet + 31 + i*9);
+	}
+
+	if (received_packet[59] == 0xff && received_packet[60] == 0xff) {
+		state->atmospheric_pressure = DEVICE_INCORRECT_PRESSURE;
+	} else {
+		state->atmospheric_pressure = received_packet[59] + received_packet[60]*256;
+	}
+}
+
+
 // Main program logic
 void process_incoming_packet(int udp_socket, const struct sockaddr_in *packet_source,
 		const unsigned char *received_packet, const size_t received_packet_size)
 {
-
 	dump_incoming_packet(stderr, packet_source, received_packet, received_packet_size);
 
+	if (received_packet_size < 20) {
 #if defined(CONFIG_REPLY_TO_PING_PACKETS) && (CONFIG_REPLY_TO_PING_PACKETS == 1)
 	// See description in config.h
 	// TODO: check the contents of the packet, not just its size
-	if (received_packet_size < 20) {
-		reply_to_ping_packet(udp_socket, packet_source, received_packet, received_packet_size);
-	}
+		reply_to_ping_packet(udp_socket, packet_source,
+				received_packet, received_packet_size);
 #endif
+	} else if (received_packet_size >= 65) {
+
+		struct device_sensor_state *sensor_state;
+		sensor_state = malloc(sizeof(struct device_sensor_state));
+
+		decode_sensor_state(sensor_state, received_packet, received_packet_size);
+
+		free(sensor_state);
+	}
 }
--- meteo_sp73.h
+++ meteo_sp73.h
@@ -7,10 +7,48 @@
 # define _POSIX_C_SOURCE
 #endif
 
+#include <stdbool.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
 #include <netinet/in.h>
+
+struct device_single_measurement {
+	float temperature;
+	// Raw temperature as sent by the device - for reverse engineering
+	uint16_t raw_temperature;
+	short unsigned int humidity;
+};
+
+//I prever not to use NaNs because of possible portability issues across architectures.
+#define DEVICE_INCORRECT_TEMPERATURE -999.0
+#define DEVICE_IS_INCORRECT_TEMPERATURE(x) (x < -990.0)
+#define DEVICE_INCORRECT_HUMIDITY -1
+
+struct device_single_sensor_data {
+	bool any_data_present;
+
+	struct device_single_measurement current;
+
+	// One data packet as sent by the device contains three sets of
+	// measurements for every sensor.
+	// One of them is described in the reverse engineering documentation as
+	// containing most recent data, others are described as containing
+	// some minimum or maximum values, possibly over some time period.
+	//
+	// TODO: check how these variables behave.
+	struct device_single_measurement historical1;
+	struct device_single_measurement historical2;
+};
+
+struct device_sensor_state {
+	struct device_single_sensor_data station_sensor;
+	struct device_single_sensor_data remote_sensors[3];
+	// As reported by an internal sensor in the weather station
+	unsigned int atmospheric_pressure;
+};
+#define DEVICE_INCORRECT_PRESSURE -1
+
 
 void process_incoming_packet(int udp_socket, const struct sockaddr_in *packet_source, 
 		const unsigned char *received_packet, const size_t received_packet_size);
