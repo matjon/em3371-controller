7c42087e915c2013abd7324324b1b63e1a12ecfb
Mateusz Jończyk <mat.jonczyk@o2.pl>
2020-10-31 16:25:21 +0100

    compile.sh: conditional compilation for DD-WRT
    Most people wouldn't want to compile for DD-WRT.

--- compile.sh
+++ compile.sh
@@ -7,26 +7,28 @@ gcc $COMMON_PARAMETERS $SOURCES -o ${OUTPUT_BASE}
 gcc $COMMON_PARAMETERS psychrometrics.c psychrometrics_test.c -o psychrometrics_test -lm
 
 
-# Compilation for DD-WRT - see:
-#https://github.com/mirror/dd-wrt/blob/master/src/router/Makefile.brcm26
-#https://github.com/mirror/dd-wrt/blob/master/src/router/configs/buildscripts/build_broadcom_K26.sh
+if [ "$1" == 'dd-wrt' ]; then
+        # Compilation for DD-WRT - see:
+        #https://github.com/mirror/dd-wrt/blob/master/src/router/Makefile.brcm26
+        #https://github.com/mirror/dd-wrt/blob/master/src/router/configs/buildscripts/build_broadcom_K26.sh
 
-# Using GCC 4.1.2 causes the program to hang on floating-point operations
-# Therefore I'm using the other compiler, which seems to work correctly
-#export PATH=/media/1T-noraid/dd-wrt/toolchain-mipsel_gcc4.1.2/bin:$PATH
-export PATH=/media/1T-noraid/dd-wrt/toolchain-mipsel_gcc-linaro_uClibc-0.9.32/bin:$PATH
-mipsel-linux-uclibc-gcc                                         \
-        -DBCMWPA2 -pipe -mips32 -mtune=mips32                   \
-        -fno-caller-saves  -mno-branch-likely -msoft-float      \
-        $COMMON_PARAMETERS                                      \
-        $SOURCES -o ${OUTPUT_BASE}_mipsel
+        # Using GCC 4.1.2 causes the program to hang on floating-point operations
+        # Therefore I'm using the other compiler, which seems to work correctly
+        #export PATH=/media/1T-noraid/dd-wrt/toolchain-mipsel_gcc4.1.2/bin:$PATH
+        export PATH=/media/1T-noraid/dd-wrt/toolchain-mipsel_gcc-linaro_uClibc-0.9.32/bin:$PATH
+        mipsel-linux-uclibc-gcc                                         \
+                -DBCMWPA2 -pipe -mips32 -mtune=mips32                   \
+                -fno-caller-saves  -mno-branch-likely -msoft-float      \
+                $COMMON_PARAMETERS                                      \
+                $SOURCES -o ${OUTPUT_BASE}_mipsel
 
-mipsel-linux-uclibc-gcc                                         \
-        -DBCMWPA2 -pipe -mips32 -mtune=mips32                   \
-        -fno-caller-saves  -mno-branch-likely -msoft-float      \
-        $COMMON_PARAMETERS                                      \
-        psychrometrics.c psychrometrics_test.c                  \
-        -o psychrometrics_test_mipsel -lm
+        mipsel-linux-uclibc-gcc                                         \
+                -DBCMWPA2 -pipe -mips32 -mtune=mips32                   \
+                -fno-caller-saves  -mno-branch-likely -msoft-float      \
+                $COMMON_PARAMETERS                                      \
+                psychrometrics.c psychrometrics_test.c                  \
+                -o psychrometrics_test_mipsel -lm
 
-# Additional possible GCC parameters: -minterlink-mips16 -mips16
-
+        # Additional possible GCC parameters: -minterlink-mips16 -mips16
+        #
+fi
