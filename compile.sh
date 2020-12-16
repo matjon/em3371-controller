e89769ae045d1b112b0d0803cdc6ecd7514e6636
Mateusz Jończyk <mat.jonczyk@o2.pl>
2020-12-16 15:16:51 +0100

    Change license to "GPLv2 or later"
    Should allow for better compatibility.

--- compile.sh
+++ compile.sh
@@ -1,3 +1,20 @@
+# Copyright (C) 2020 Mateusz Jończyk
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
 SOURCES="src/main.c src/emax_em3371.c src/psychrometrics.c src/output_json.c src/output_csv.c src/output_sql.c"
 OUTPUT_BASE=emax_em3371_decoder
 COMMON_PARAMETERS="-O2 -std=c99 -Wall -Wextra"
