820b63181c0c913ab443ababed791f55ad02c907
Mateusz Jończyk <mat.jonczyk@o2.pl>
2021-03-07 14:10:47 +0100

    output_sql: store generated SQL statements as an array
    Will be required for direct connection to the SQL server.

--- src/output_sql.c
+++ src/output_sql.c
@@ -20,6 +20,9 @@
 #include "output_sql.h"
 #include "main.h"
 
+#include <stdlib.h>
+#include <string.h>
+
 static FILE *sql_output_stream = NULL;
 static bool sql_output_stream_close_on_exit = false;
 
@@ -43,7 +46,8 @@ void shutdown_sql_output()
                                 ", " FORMAT, SOURCE);                   \
         }
 
-void display_sensor_state_debug(FILE *stream, const int sensor_id,
+static size_t get_single_sensor_state_debug_sql(char *output, size_t output_space,
+                const int sensor_id,
                 const struct device_single_sensor_data *sensor_data,
                 const unsigned char payload_byte_0x31)
 {
@@ -77,12 +81,12 @@ void display_sensor_state_debug(FILE *stream, const int sensor_id,
                 sensor_id == 0
                 )
 
-        fprintf(stream,
+        return snprintf(output, output_space,
                 "INSERT INTO sensor_reading_debug(metrics_state_id, sensor_id"
                 "%s%s%s%s%s"
                 ") VALUES (@insert_id, %d"
                 "%s%s%s%s%s"
-                ");\n",
+                ")",
                 temperature_min_field, temperature_max_field,
                 humidity_min_field, humidity_max_field,
                 payload_0x31_field,
@@ -94,7 +98,8 @@ void display_sensor_state_debug(FILE *stream, const int sensor_id,
                 payload_0x31_str);
 }
 
-void display_sensor_reading_sql(FILE *stream, const int sensor_id,
+static size_t get_single_sensor_state_sql(char *output, size_t output_space,
+                const int sensor_id,
                 const struct device_single_sensor_data *sensor_data,
                 uint16_t atmospheric_pressure)
 {
@@ -129,10 +134,12 @@ void display_sensor_reading_sql(FILE *stream, const int sensor_id,
                 battery_low_str = ", b\'0\'";
         }
 
-        fprintf(stream, "INSERT INTO sensor_reading(metrics_state_id, sensor_id"
+
+        return snprintf(output, output_space,
+                "INSERT INTO sensor_reading(metrics_state_id, sensor_id"
                 "%s%s%s%s, battery_low"
                 ") VALUES (@insert_id, %d"
-                "%s%s%s%s%s);\n",
+                "%s%s%s%s%s)",
                 temperature_field, humidity_field,
                 dew_point_field, atmospheric_pressure_field,
 
@@ -142,7 +149,78 @@ void display_sensor_reading_sql(FILE *stream, const int sensor_id,
                 battery_low_str);
 }
 
-void display_sensor_state_sql(FILE *stream, const struct device_sensor_state *state)
+bool sql_statements_list_construct(struct sql_statements_list *statements)
+{
+        statements->count=0;
+        statements->statements = malloc(SQL_STATEMENTS_MAX_COUNT
+                        * sizeof(statements->statements[0]));
+        if (statements->statements == NULL) {
+                return false;
+        }
+        statements->max_count = SQL_STATEMENTS_MAX_COUNT;
+
+        statements->memory = malloc(SQL_STATEMENTS_MEMORY_SIZE);
+        if (statements->memory == NULL) {
+                free(statements->statements);
+                statements->statements = NULL;
+
+                return false;
+        }
+
+        statements->memory_left = SQL_STATEMENTS_MEMORY_SIZE;
+        statements->next_statement_place = statements->memory;
+        return true;
+}
+
+void sql_statements_list_free(struct sql_statements_list *statements)
+{
+        free(statements->statements);
+        statements->statements = NULL;
+
+        free(statements->memory);
+        statements->memory = NULL;
+
+        statements->max_count = 0;
+        statements->memory_left = 0;
+        statements->next_statement_place = NULL;
+}
+
+bool sql_statements_list_arrange_next(struct sql_statements_list *statements)
+{
+        if (statements->count >= statements->max_count) {
+                return false;
+        }
+
+        if (statements->memory_left == 0) {
+                return false;
+        }
+
+        size_t length = strlen(statements->next_statement_place);
+        if (length+1 > statements->memory_left) {
+                fputs("Error: memory corruption detetcted in "
+                        "sql_statements_list_arrange_next.\n", stderr);
+                abort();
+        }
+        statements->memory_left -= (length+1);
+
+        if (length == 0) {
+                return true;
+        }
+
+        statements->statements[statements->count++] = statements->next_statement_place;
+        statements->next_statement_place += (length+1);
+
+        if (statements->memory_left == 0) {
+                statements->next_statement_place = NULL;
+        }
+
+        return true;
+}
+
+
+void get_sensor_state_sql(
+                struct sql_statements_list *statements,
+                const struct device_sensor_state *state)
 {
 	char current_time[30];
 	current_time_to_string(current_time, sizeof(current_time), false);
@@ -160,31 +238,65 @@ void display_sensor_state_sql(FILE *stream, const struct device_sensor_state *st
 //
 //      When the data disappears when zooming in, this is likely to be a timezone issue.
 
-        fprintf(stream, "START TRANSACTION;\n");
-        fprintf(stream, "INSERT INTO metrics_state(time_utc, device_time) "
-                        "VALUES ('%s', '%s'); \n",
-                        current_time, device_time_str);
+        snprintf(statements->next_statement_place,
+                statements->memory_left,
+                "INSERT INTO metrics_state(time_utc, device_time) "
+                "VALUES ('%s', '%s')",
+                current_time, device_time_str);
+        sql_statements_list_arrange_next(statements);
 
-        fprintf(stream, "SET @insert_id = LAST_INSERT_ID();\n");
+        snprintf(statements->next_statement_place,
+                statements->memory_left,
+                "SET @insert_id = LAST_INSERT_ID()");
+        sql_statements_list_arrange_next(statements);
+
         if (state->station_sensor.any_data_present) {
-                display_sensor_reading_sql(stream, 0,
-                                &state->station_sensor,
+                get_single_sensor_state_sql(
+                                statements->next_statement_place,
+                                statements->memory_left,
+                                0, &state->station_sensor,
                                 state->atmospheric_pressure);
-                display_sensor_state_debug(stream, 0, &state->station_sensor,
+                sql_statements_list_arrange_next(statements);
+
+                get_single_sensor_state_debug_sql(
+                                statements->next_statement_place,
+                                statements->memory_left,
+                                0, &state->station_sensor,
                                 state->payload_byte_0x31);
+                sql_statements_list_arrange_next(statements);
         }
         for (int i=0; i<3; i++) {
                 if (state->remote_sensors[i].any_data_present) {
-                        display_sensor_reading_sql(stream, i+1,
+                        get_single_sensor_state_sql(
+                                statements->next_statement_place,
+                                statements->memory_left,
+                                i+1,
                                 &state->remote_sensors[i],
                                 DEVICE_INCORRECT_PRESSURE);
 
-                        display_sensor_state_debug(stream, i+1, &state->remote_sensors[i], 0);
+                        sql_statements_list_arrange_next(statements);
+                        get_single_sensor_state_debug_sql(
+                                statements->next_statement_place,
+                                statements->memory_left,
+                                i+1, &state->remote_sensors[i], 0);
+                        sql_statements_list_arrange_next(statements);
                 }
         }
+}
+
+void display_sensor_state_sql(FILE *stream, const struct device_sensor_state *state)
+{
+        struct sql_statements_list statements;
+
+        fprintf(stream, "START TRANSACTION;\n");
+
+        sql_statements_list_construct(&statements);
+        get_sensor_state_sql(&statements, state);
+        for (unsigned i = 0; i < statements.count; i++) {
+                fprintf(stream, "%s;\n", statements.statements[i]);
+        }
+        sql_statements_list_free(&statements);
 
         fprintf(stream, "COMMIT;\n");
-
         fflush(stream);
-
 }
--- src/output_sql.h
+++ src/output_sql.h
@@ -20,4 +20,24 @@
 #include <stdio.h>
 #include "emax_em3371.h"
 
+struct sql_statements_list {
+        unsigned int count;
+        char **statements;
+
+// private
+        unsigned int max_count;
+        char *memory;
+        char *next_statement_place;
+        size_t memory_left;
+};
+#define SQL_STATEMENTS_MAX_COUNT 20
+#define SQL_STATEMENTS_MEMORY_SIZE 8*1024
+
+bool sql_statements_list_construct(struct sql_statements_list *statements);
+void sql_statements_list_free(struct sql_statements_list *statements);
+
+/*
+void sql_statements_list_get(struct sql_statements_list *statements,
+                const struct device_sensor_state *state);
+*/
 void display_sensor_state_sql(FILE *stream, const struct device_sensor_state *state);
